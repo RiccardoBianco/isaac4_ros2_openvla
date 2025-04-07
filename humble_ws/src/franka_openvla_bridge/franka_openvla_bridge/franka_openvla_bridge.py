62e6d9c516812839ea48e5e80e01ef75a4afd3b7
Camilla Mazzoleni <109732478+CamillaMazzoleni@users.noreply.github.com>
2025-04-07 11:22:08 +0200

    Update franka_openvla_bridge.py

--- humble_ws/src/franka_openvla_bridge/franka_openvla_bridge/franka_openvla_bridge.py
+++ humble_ws/src/franka_openvla_bridge/franka_openvla_bridge/franka_openvla_bridge.py
@@ -29,7 +29,7 @@ class FrankaOpenVLABridge(Node):
 
         self.joint_pub = self.create_publisher(
             JointState,
-            '/joint_command',  # controller topic
+            '/gripper/joint_commands',  # controller topic
             10)
 
         self.marker_pub = self.create_publisher(
